d00c60b2f48c26f7fa4b2cfe51bd9ae27676dddc
nealmanaktola <nealmanaktola@gmail.com>
2015-03-24 13:05:25 -0400

    initial commit

---
+++ app/src/main/java/com/knowroaming/specialsms/SMSReceiver.java
@@ -0,0 +1,47 @@
+package com.knowroaming.specialsms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+/**
+ * Created by Neal on 3/20/2015.
+ */
+public class SMSReceiver extends BroadcastReceiver {
+
+    private void getMessageBody(SmsMessage[] msgs, Object[] pdus) {
+        String msg_from;
+
+        for (int i = 0; i < msgs.length; i++) {
+            msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+            msg_from = msgs[i].getOriginatingAddress();
+            String msgBody = msgs[i].getMessageBody();
+            Log.d("SMS Recieved", msgBody);
+        }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+
+        if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
+            Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
+            SmsMessage[] msgs = null;
+            String msg_from;
+
+            if (bundle != null){
+                //---retrieve the SMS message received---
+                try{
+                    Object[] pdus = (Object[]) bundle.get("pdus");
+                    msgs = new SmsMessage[pdus.length];
+                    getMessageBody(msgs,pdus);
+                }catch(Exception e){
+                            Log.d("Exception caught", e.getMessage());
+                }
+            }
+        }
+    }
+}
